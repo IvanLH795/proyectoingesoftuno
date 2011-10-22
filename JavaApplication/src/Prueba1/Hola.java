7e3eed686bcc2a55abf35b8ebdc9541ef7262a3a
Beltran <Beltran@Beltran-PC>
2011-10-21 20:48:38 -0500

    Prueba1

---
+++ JavaApplication/src/Prueba1/Hola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Prueba1;
+
+/**
+ *
+ * @author Beltran
+ */
+public class Hola {
+
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+}
+
