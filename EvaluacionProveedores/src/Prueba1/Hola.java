6ca6958739c28103c649f2f3a32178e4906d6897
Darthian <Darthian@Darthian-PC>
2011-10-23 20:11:32 -0500

    primer commit

---
+++ EvaluacionProveedores/src/Prueba1/Hola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Prueba1;
+
+/**
+ *
+ * @author Beltran
+ */
+public class Hola {
+
+  public static void main(String[] args) {
+      Hola2 prueba2 = new Hola2();
+    prueba2.print();
+  }
+}
+
