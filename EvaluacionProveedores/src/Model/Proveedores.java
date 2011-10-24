59d72f2bfe07f8b4d1d6f67a5b51a1d3ddc8f2d6
Darthian <Darthian@Darthian-PC>
2011-10-24 15:10:12 -0500

    Clases

---
+++ EvaluacionProveedores/src/Model/Proveedores.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Darthian
+ */
+public class Proveedores {
+
+}
