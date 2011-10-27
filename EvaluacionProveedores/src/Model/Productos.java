8c84af164ebc1c1089bee0815e9cde3facf44c56
Beltran <Beltran@Beltran-PC>
2011-10-27 16:34:00 -0500

    Producto

--- EvaluacionProveedores/src/Model/Productos.java
+++ EvaluacionProveedores/src/Model/Productos.java
@@ -10,5 +10,26 @@ package Model;
  * @author Darthian
  */
 public class Productos {
+private String nombreProducto;
+private Float dineroDisponible;
+
+    public Productos() {
+    }
+
+    public Float getDineroDisponible() {
+        return dineroDisponible;
+    }
+
+    public void setDineroDisponible(Float dineroDisponible) {
+        this.dineroDisponible = dineroDisponible;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
 
 }
