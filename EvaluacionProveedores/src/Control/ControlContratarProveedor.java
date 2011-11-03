2e4635854e643b9c0200f3d63e25ae4348a67bac
Darthian <Darthian@Darthian-PC>
2011-11-03 14:15:44 -0500

    contratar

--- EvaluacionProveedores/src/Control/ControlContratarProveedor.java
+++ EvaluacionProveedores/src/Control/ControlContratarProveedor.java
@@ -5,10 +5,26 @@
 
 package Control;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
 /**
  *
  * @author Darthian
  */
 public class ControlContratarProveedor {
-    
+    Document documento = new Document();
+        try {
+            PdfWriter.getInstance(documento, new FileOutputStream("LuAuF.pdf"));
+            documento.open();
+            documento.add(new Paragraph("http://luauf.com/"));
+        } catch (DocumentException de) {
+            System.err.println(de.getMessage());
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        documento.close();
 }
