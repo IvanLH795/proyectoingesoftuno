47f49f225ee1874ed6f15e4d16da6bc01bdeb783
Darthian <Darthian@Darthian-PC>
2011-11-03 14:33:32 -0500

    contratar pdf

--- EvaluacionProveedores/src/Frontera/ContratarProveedor.java
+++ EvaluacionProveedores/src/Frontera/ContratarProveedor.java
@@ -319,6 +319,7 @@ public class ContratarProveedor extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         Proveedores proveedor = new Proveedores();//Proveedor seleccionado con el que se hara contrato
         ControlContratarProveedor contrato = new ControlContratarProveedor(proveedor);
+        contrato.generarContrato();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
