12bca387bbcf6b245478817a9636948ebac392d0
Camilo <Camilo@personal-camilo>
2011-11-05 18:46:08 -0500

    itextpdf agregado al path.

--- EvaluacionProveedores/src/Frontera/GestorPresupuesto.java
+++ EvaluacionProveedores/src/Frontera/GestorPresupuesto.java
@@ -244,6 +244,7 @@ public class GestorPresupuesto extends javax.swing.JFrame {
 
     private void btnProductoAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProductoAgregarActionPerformed
         AñadirProducto aproducto=new AñadirProducto();
+        aproducto.setLocationRelativeTo(null);
         aproducto.setVisible(true);
         
         /*Vector obj = new Vector();
