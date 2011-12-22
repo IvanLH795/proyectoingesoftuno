c3e634f865584b3503c4c1505432206fdd36c2f9
USUARIO <USUARIO@USUARIO-PC>
2011-12-22 12:10:34 -0500

    Bug borrar Producto que no existe corregido.

--- EvaluacionProveedores/src/Frontera/MiModelo.java
+++ EvaluacionProveedores/src/Frontera/MiModelo.java
@@ -10,12 +10,14 @@ import javax.swing.table.DefaultTableModel;
 public class MiModelo extends DefaultTableModel
 {
    /** Primera columna Boolean, segunda Float y el resto Float */
+    @Override
    public Class getColumnClass(int columna)
    {
       if (columna == 0) return String.class;
       if (columna == 1) return Float.class;
       return Float.class;
    }
+    @Override
    public boolean isCellEditable(int row, int column){
         return false;
    }
